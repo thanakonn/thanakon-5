5142e304950bdf8f6ad04237e36f03b9a76ee4dd
thanakonn <153165057+thanakonn@users.noreply.github.com>
2023-12-21 14:42:13 +0700

    Add files via upload
    5,34

---
+++ pythonProject1/DS.py
@@ -0,0 +1,15 @@
+import pandas as pd
+data = pd.read_csv('salary_dataset.csv')
+print(data)
+print(data.info())
+print(data.describe())
+print("mean age = ",data['age'].mean())
+print("mean gpe = ",data['age'].mean())
+print("mean salary = ",data['salary'].max())
+print("mean experience = ",data['experience'].min())
+print(data.head())
+print(data.tail())
+print(data.iloc[30:41])
+print(data['age'].unique)
+dataAge26=data[data['age']==26]
+print(dataAge26)
